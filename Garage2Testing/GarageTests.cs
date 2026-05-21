1ea0cd393eefe7902ecde3b369a7b2ccc13027c6
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-21 11:21:47 +0200

    moved common vars as pre-run, adjusted tests where possible

--- Garage2Testing/GarageTests.cs
+++ Garage2Testing/GarageTests.cs
@@ -8,70 +8,55 @@ namespace Garage2Testing;
 
 public class GarageTests
 {
+    private readonly Vehicle[] _smallVehicleSet =
+    [
+        new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
+        new(VehicleTypes.Motorcycle, "BIKE42", new FuelEngine(85, 0.6m, FuelTypes.Gasoline), "Blue"),
+        new(VehicleTypes.Bus, "TRK999", new FuelEngine(500, 12.7m, FuelTypes.Diesel), "Orange"),
+    ];
+    private readonly Garage<Vehicle> _garage;
+
+    public GarageTests()
+    {
+        _garage = new(5, _smallVehicleSet);
+    }
+
     [Fact]
     public void InitializeGarage_WithData()
     {
-        Vehicle vehicle = new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White");
-        Vehicle[] expected = [ new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White") ];
-
-        Garage<Vehicle> garage = new(10, [vehicle]);
-
-        Assert.Equal(expected, [.. garage]);
-        Assert.Equal(1, garage.Length);
-        Assert.Equal(1, garage.Count());
-        Assert.Equal(10, garage.Capacity);
-        Assert.Equal(1, garage.TypesCount[VehicleTypes.Car]);
+        Assert.Equal(_smallVehicleSet, _garage);
+        Assert.Equal(3, _garage.Length);
+        Assert.Equal(3, _garage.Count());
+        Assert.Equal(5, _garage.Capacity);
+        Assert.Equal(1, _garage.TypesCount[VehicleTypes.Car]);
+        Assert.Equal(1, _garage.TypesCount[VehicleTypes.Motorcycle]);
+        Assert.Equal(1, _garage.TypesCount[VehicleTypes.Bus]);
     }
 
     [Fact]
     public void InitializeGarage_WithTooMuchData_ThrowsDatasetTooLargeException()
     {
-        Vehicle[] vehicles = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Motorcycle, "BIKE42", new FuelEngine(85, 0.6m, FuelTypes.Gasoline), "Blue"),
-            new(VehicleTypes.Bus, "TRK999", new FuelEngine(500, 12.7m, FuelTypes.Diesel), "Orange"),
-        ];
-
-        var ex = Assert.Throws<DatasetTooLargeException>(() => new Garage<Vehicle>(1, vehicles));
+        var ex = Assert.Throws<DatasetTooLargeException>(() => new Garage<Vehicle>(1, _smallVehicleSet));
         Assert.Equal("Dataset too large: 3. Max size: 1", ex.Message);
     }
 
     [Fact]
     public void RemovingVehicle_ReducesAmounts()
     {
-        Vehicle[] vehicles = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Motorcycle, "BIKE42", new FuelEngine(85, 0.6m, FuelTypes.Gasoline), "Blue"),
-            new(VehicleTypes.Bus, "TRK999", new FuelEngine(500, 12.7m, FuelTypes.Diesel), "Orange"),
-        ];
-        Vehicle[] expected = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Bus, "TRK999", new FuelEngine(500, 12.7m, FuelTypes.Diesel), "Orange"),
-        ];
+        Vehicle[] expected = [ _smallVehicleSet[0], _smallVehicleSet[2]];
+        _garage.Remove("BIKE42");
 
-        Garage<Vehicle> garage = new(5, vehicles);
-
-        garage.Remove("BIKE42");
-        var result = garage.ToArray();
-
-        Assert.Equal(expected, result);
-        Assert.Equal(2, garage.Length);
-        Assert.Equal(0, garage.TypesCount[VehicleTypes.Motorcycle]);
-        Assert.Equal(1, garage.TypesCount[VehicleTypes.Car]);
-        Assert.Equal(1, garage.TypesCount[VehicleTypes.Bus]);
+        Assert.Equal(expected, _garage);
+        Assert.Equal(2, _garage.Length);
+        Assert.Equal(1, _garage.TypesCount[VehicleTypes.Car]);
+        Assert.Equal(0, _garage.TypesCount[VehicleTypes.Motorcycle]);
+        Assert.Equal(1, _garage.TypesCount[VehicleTypes.Bus]);
     }
 
     [Fact]
     public void Garage_RemovingNonExistentVehicle_ThrowsArgumentException()
     {
-        Vehicle[] vehicles = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Motorcycle, "BIKE42", new FuelEngine(85, 0.6m, FuelTypes.Gasoline), "Blue"),
-            new(VehicleTypes.Bus, "TRK999", new FuelEngine(500, 12.7m, FuelTypes.Diesel), "Orange"),
-        ];
-        Garage<Vehicle> garage = new(5, vehicles);
-
-        Assert.Throws<ArgumentException>(() => garage.Remove("NOTHERE"));
+        Assert.Throws<ArgumentException>(() => _garage.Remove("NOTHERE"));
     }
 
     [Fact]
@@ -110,28 +95,12 @@ public class GarageTests
     [Fact]
     public void Garage_DeacreasingCapacityBelowLength_ThrowsArgumentException()
     {
-        Vehicle[] vehicles = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Car, "XYZ789", new FuelEngine(320, 3.0m, FuelTypes.Diesel), "Black"),
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-        ];
-        Garage<Vehicle> garage = new(5, vehicles);
-
-        Assert.Throws<ArgumentException>(() => garage.Resize(1));
+        Assert.Throws<ArgumentException>(() => _garage.Resize(1));
     }
 
     [Fact]
     public void Garage_IEnumerableToArray_Converion()
     {
-        Vehicle[] vehicles = [
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-            new(VehicleTypes.Car, "XYZ789", new FuelEngine(320, 3.0m, FuelTypes.Diesel), "Black"),
-            new(VehicleTypes.Car, "EV0001", new ElectricEngine(408, 100.0m), "White"),
-        ];
-        Garage<Vehicle> garage = new(5, vehicles);
-
-        var result = garage.ToArray();
-
-        Assert.Equal(vehicles, result);
+        Assert.Equal(_smallVehicleSet, _garage.ToArray());
     }
 }
